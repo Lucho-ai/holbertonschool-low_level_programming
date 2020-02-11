067b071d0f1f9b9c0c2c1c7aa412f792817c40a5
Lucho-ai <luchoalejomarin1999@gmail.com>
2020-02-11 21:28:56 +0000

    Add funtion

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * add - check the code for Holberton School students.
+ * @a: parameter to check
+ * @b: parameter to check
+ * Return: add
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
