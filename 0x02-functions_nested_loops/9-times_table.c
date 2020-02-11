17e6a05cfe45dc4ea0a992bbb88b7a4069f75582
Lucho-ai <luchoalejomarin1999@gmail.com>
2020-02-11 21:18:18 +0000

    modified code and add spaces

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,11 +15,11 @@ void times_table(void)
 		{
 			if ((a * b) / 10 != 0)
 				_putchar(((a * b) / 10) + '0');
-			else if ((a * b) %10 != 0)
+			else if ((a * b) % 10 != 0)
 				_putchar(' ');
 			else if (a == 0 && b > 0)
 				_putchar(' ');
-			_putchar(((a *b) % 10) + '0');
+			_putchar(((a * b) % 10) + '0');
 			if (b < 9)
 			{
 				_putchar(',');
