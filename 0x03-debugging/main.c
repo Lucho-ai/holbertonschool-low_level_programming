3a7319671139fd3ba19c0d5f77a123269b20099c
Lucho-ai <luchoalejomarin1999@gmail.com>
2020-02-12 01:43:43 +0000

    re

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int f;
+
+	f = 98;
+	positive_or_negative(f);
+
+	return (0);
+}
+
