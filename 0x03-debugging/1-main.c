46bc68aeab2736ed70d11eff97271c7a4bdfd617
Lucho-ai <luchoalejomarin1999@gmail.com>
2020-02-12 02:01:45 +0000

    largest number

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int r;
+
+	printf("Infinite loop incoming :(\n");
+
+	r = 0;
+	/**
+	 *while (r < 10)
+	 *{
+	 *putchar(r);
+	 *}
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
