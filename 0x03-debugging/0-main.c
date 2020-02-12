9682c81c1398f3942c06d21c4c644caaaca8e5b2
Lucho-ai <luchoalejomarin1999@gmail.com>
2020-02-12 01:34:16 +0000

    Fix

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *main -gives us a random number and tells if it is positive, negative or cero
+ *Return: 0 if success
+*/
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
