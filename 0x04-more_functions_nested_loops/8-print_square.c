983429c9b10085e6ec1e616e0b7d30f8967c79ae
Lucho-ai <luchoalejomarin1999@gmail.com>
2020-02-13 22:04:10 +0000

    Print squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+* print_square - check the code fot holberton school studets
+* @n: num 1
+*/
+void print_square(int size)
+{
+	int square;
+	int espacio;
+
+	if (size > 0)
+	{
+		for (square = 1; square <= size; square++)
+		{
+			for (espacio = 1; espacio < size; espacio++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+
+	}
+
+	else
+		_putchar('\n');
+}
