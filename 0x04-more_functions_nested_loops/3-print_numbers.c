8a97009b661b4a4411c1e113f148ddf2610a189a
Lucho-ai <luchoalejomarin1999@gmail.com>
2020-02-13 17:25:17 +0000

    Print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,20 +1,16 @@
 #include "holberton.h"
 /**
  * print_numbers - check the code for Holberton School students.
- * @a: parameter to check
- * Return: Always 0
  */
 
 void print_numbers(void)
 {
-	int a = 0;
+        int i;
 
-	if (a < 9)
-	{
-		for (; a <= '9'; a++)
-		{
-			_putchar([a]);
-		}
-	}
-	_putchar('\n');
+        for (i = 0; i <= 9; i++)
+        {
+                _putchar(i + '0');
+        }
+
+        _putchar('\n');
 }
