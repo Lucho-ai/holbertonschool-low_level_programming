9082f846f7a35c25a4dd20879bf4416e09594c9e
Lucho-ai <luchoalejomarin1999@gmail.com>
2020-02-18 21:27:53 +0000

    Funtion puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * reset_to_98 - funtion that takes a parameter and update.
+ * @n: its the value of the pointer
+ * Return: Always 0.
+ */
+
+void _puts(char *str)
+{
+	int c = 0;
+
+	while (str[c] != '\0')
+	{
+		_putchar(str[c]);
+		c++;
+	}
+	_putchar('\n');
+}
