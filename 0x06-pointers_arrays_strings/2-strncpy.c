e38f619dc78b624617ad2fecc4118dd298b57353
Lucho-ai <luchoalejomarin1999@gmail.com>
2020-02-20 21:40:55 +0000

    Strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * _strcat - funtion that print a string overwriting a string
+ * @src: its the value for string
+ * @dest: its the value of the array
+ * Return: Always dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+        	dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
