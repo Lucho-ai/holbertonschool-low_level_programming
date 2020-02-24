da9e4dba1ae2cfc1c581663591aa43d1f7ed55a7
Lucho-ai <luchoalejomarin1999@gmail.com>
2020-02-24 17:00:48 +0000

    Memcpy file

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	
