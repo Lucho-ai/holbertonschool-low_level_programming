c4e090bdf4ac5e0e6eb8a8292544ef1777efaefa
Lucho-ai <luchoalejomarin1999@gmail.com>
2020-02-25 00:19:30 +0000

    Strstr.c fix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,18 +8,19 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j, p;
+	int i;
+	int j;
+	int p;
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (p = i, j = 0; haystack[j] != '\0'; j++, p++)
+		for (p = i, j = 0; needle[j] != '\0'; j++, p++)
 		{
-			if (haystack[p] == needle[j] || haystack[p] == '\0')
+			if (haystack[p] != needle[j] || haystack[p] == '\0')
 			{
 				break;
 			}
 		}
-
 		if (needle[j] == '\0')
 		{
 			return (haystack + i);
