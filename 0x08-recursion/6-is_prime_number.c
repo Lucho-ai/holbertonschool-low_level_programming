3af01c10b6d7fca07778e516b7f2e41156d76147
Lucho-ai <luchoalejomarin1999@gmail.com>
2020-02-26 23:13:34 +0000

    Prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number - function to calculate if n is prime.
+ * @n: Check if prime
+ * Return: prime number
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (_find(n, 2));
+}
+
+/**
+ * _find - function that help to find a prime number.
+ * @n: number to be calculate
+ * @find1: Help find if its prime
+ * Return: 1 if prime; 0 if not prime.
+ */
+
+int _find(int j, int find1)
+{
+	if (find1 <= (j - 1))
+	{
+		if (j % find1 == 0)
+		{
+			return (0);
+		}
+		return (_find(j, find1 + 1));
+	}
+	return (1);
+}
