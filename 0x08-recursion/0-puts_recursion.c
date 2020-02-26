01a71b52fa68df00860eb21abef9d6216fae173c
Lucho-ai <luchoalejomarin1999@gmail.com>
2020-02-26 18:16:18 +0000

    Puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * _puts_recursion - prints a string.
+ * @s: string.
+ */
+void _puts_recursion(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
