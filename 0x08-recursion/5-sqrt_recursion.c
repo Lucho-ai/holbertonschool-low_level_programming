c1f549437d454128b90e408ad01652cb54395023
Lucho-ai <luchoalejomarin1999@gmail.com>
2020-02-26 22:44:21 +0000

    Fix in the code

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,36 +1,31 @@
 #include "holberton.h"
-#include <stido.h>
 /**
- * _sqrt_recursion - returns the natural square root of a number.
- * @n: Int.
+ * _sqrt_recursion - Function returns the natural square root of a number.
+ * @n: Integer.
  * Return: Result of the square.
  */
 int _sqrt_recursion(int n)
 {
 	int result = 0;
 
-	result = square(n, 0);
+	result = _find (n, 0);
 	return (result);
 }
 
 /**
- * _square - Find the square.
- * @j: integer
- * @i:integer
+ * _find - Find the square.
+ * @n: integer
+ * @xD:integer
  * Return: result of the sqare root of n
  */
-int _square(int j, int i)
+int _find(int j, int i)
 {
 	int result = 0;
 
 	if (i * i > j)
-	{
 		return (-1);
-	}
 	if (i * i == j)
-	{
 		return (i);
-		result = _square(j, i + 1);
-		return (result);
-	}
+	result = _find(j, i + 1);
+	return (result);
 }
