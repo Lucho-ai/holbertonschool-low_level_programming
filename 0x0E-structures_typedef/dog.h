62e373ec50b185cac7a650d84db22ba35cf8d2c9
Lucho-ai <luchoalejomarin1999@gmail.com>
2020-03-09 22:10:51 +0000

    Wuafs

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOGH
+#define DOGH
+/**
+ *struct dog - dof
+ *@name: The name of the dog.
+ *@age: The age of the dog.
+ *@owner: The owner of the dog.
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+typedef struct dog
+dog_t;
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
