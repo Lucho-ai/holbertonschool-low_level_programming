e6d71074e8a9565ac38fe8c87aadbd748079cfb8
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-03-20 15:04:24 -0500

    Create 2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * add_node - Adds nodes at the beginning of list_t list
+ * @head: Pointer to head.
+ * @str: Pointer to str.
+ * Return: The elements it prints.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	int j;
+	list_t *a = malloc(sizeof(list_t));
+
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; str[i] != '\0')
+	{
+		i++;
+	}
+	a->str = strdup(str);
+	a->len = j;
+	a->next = *head;
+	*head = a;
+	return (a);
+}
