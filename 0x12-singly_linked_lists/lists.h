3419e66ffd7addce8111f4483b882b221357402f
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-03-20 12:05:59 -0500

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -9,7 +9,7 @@
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
- * Description: singly linked list node structure
+ * Description: linked list node structure
  */
 
 typedef struct list_s
