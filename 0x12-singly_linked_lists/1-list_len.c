ef44fabbf10f48c4df00211c2570f1dd00f413f9
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-03-20 14:55:03 -0500

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,22 +1,17 @@
 #include "lists.h"
 
 /**
- * list_len - Length of a linked list
- * @h: Pointer of the linked list
+ * list_len - Calculates the length of a linked list
+ * @h: Pointer to the head of the linked list
  * Return: Amount of nodes in the linked list
- * @a: Variable to iterate linked list
  */
 size_t list_len(const list_t *h)
 {
-	int a;
+	int i;
 
 	if (h == NULL)
-	{
-    return (0);
-  }
-	for (a = 1; h->next != NULL; a++)
-  {
-    h = h->next;
-  }
-	return (a);
+		return (0);
+	for (i = 1; h->next != NULL; i++)
+		h = h->next;
+	return (i);
 }
