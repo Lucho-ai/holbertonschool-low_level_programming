abfb1b11634219fa86cf9919a53f250fa3153b02
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-03-25 13:35:12 -0500

    Create 8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * sum_listint - Sum of all data(n).
+ *@head: Head.
+ * Return: address of nodes.
+ */
+int sum_listint(listint_t *head)
+{
+	int a;
+	a = 0;
+
+	if (!head)
+		return (a);
+
+	if (head->next)
+		a += sum_listint(head->next);
+
+	a += head->n;
+	return (a);
+}
