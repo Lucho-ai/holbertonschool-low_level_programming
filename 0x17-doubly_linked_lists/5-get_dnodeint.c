43de54320165b68e5d5f41f6d61856e65095d16b
Lucho-ai <luchoalejomarin1999@gmail.com>
2020-09-07 02:50:11 +0000

    Get list

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,10 +1,12 @@
 #include "lists.h"
+
 /**
  * get_dnodeint_at_index - Returns the nth node of a list.
  * @head: Head of the list.
  * @index: Place of the list.
  * Return: Head or NULL.
  */
+
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int counter;
